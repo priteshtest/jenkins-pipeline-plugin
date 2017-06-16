e043ac401573fbfd36c85dfd2a8314d34e04a25b
priteshtest <mailtopdemehta@gmail.com>
2017-06-16 13:57:12 +0800

    global variable

---
+++ vars/acme.groovy
@@ -0,0 +1,13 @@
+class acme implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
+
