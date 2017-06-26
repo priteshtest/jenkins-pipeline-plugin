5a38bdc12221091d66396f9eca39156af8d49da9
priteshtest <mailtopdmehta@gmail.com>
2017-06-26 15:06:07 +0800

    Update runBuildScripts.groovy

--- vars/runBuildScripts.groovy
+++ vars/runBuildScripts.groovy
@@ -1,9 +1,14 @@
 def call(bodyBlock) {
   
   def config = [
-      test_script: './test'
+      test_script: './test',
   ]
+  bodyBlock.resolveStrategy = Closure.DELEGATE_FIRST
+  bodyBlock.delegate = config
+  bodyBlock()
+  final String myTests = config.test_script
   stage('Run Tests') {
       sh 'echo "Run Test Script"'
+      println(myTests)
   }
 }
