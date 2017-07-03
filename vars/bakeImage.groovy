be8e39c530cda790ee947f295918e8a22b1af13a
priteshtest <mailtopdmehta@gmail.com>
2017-07-03 14:54:50 +0800

    Update bakeImage.groovy

--- vars/bakeImage.groovy
+++ vars/bakeImage.groovy
@@ -1,6 +1,6 @@
 def call(String dockerImg) {
     stage('building docker image :'+dockerImg) {
         //sh 'echo "docker build -t ${dockerImg}" '
-        println(DOCKER_IMAGE)
+        println(env.DOCKER_IMAGE)
     }
 }
