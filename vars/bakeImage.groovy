691712526f585a2c1d729902c9aa61ec68917cdd
priteshtest <mailtopdmehta@gmail.com>
2017-07-19 15:38:36 +0800

    Update bakeImage.groovy

--- vars/bakeImage.groovy
+++ vars/bakeImage.groovy
@@ -1,6 +1,7 @@
 def call(String dockerImg) {
-    stage('building docker image :'+dockerImg) {
+    println("Hellow World!")
+    //stage('building docker image :'+dockerImg) {
         //sh 'echo "docker build -t ${dockerImg}" '
-        println(env.DOCKER_IMAGE)
-    }
+       // println(env.DOCKER_IMAGE)
+    //}
 }
