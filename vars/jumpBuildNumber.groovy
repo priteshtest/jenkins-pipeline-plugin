805a691d251b67d8cef9a91a5e068d038f916985
Pritesh <pritesh@sourceclear.com>
2017-07-19 15:21:30 +0800

    Update jumpBuildNumber.groovy

--- vars/jumpBuildNumber.groovy
+++ vars/jumpBuildNumber.groovy
@@ -4,12 +4,20 @@ def call(configMap) {
     }
 	final String oldJobName = configMap.getOrDefault("old_job_name", '')
 	final String newJobName = configMap.getOrDefault("new_job_name", '')
-	if (oldJobName != '') {
-		job = Jenkins.instance.getItemByFullName(oldJobName)
-		nextBuildNumber=job.getNextBuildNumber()
-		//Update next build number only for 'master' branch job as BakeIamge runs only for master/develop branch
-		job = Jenkins.instance.getItemByFullName(newJobName+"/master")
-		job.nextBuildNumber = nextBuildNumber
-		job.saveNextBuildNumber()
-	} 
+	
+	if (oldJobName == null || oldJobName.trim() == "") {
+        	throw new NullPointerException("The old_job_name cannot be blank or null!")
+	}	
+	
+	if (newJobName == null || newJobName.trim() == "") {
+        	throw new NullPointerException("The new_job_name cannot be blank or null!")
+	}	
+
+	job = Jenkins.instance.getItemByFullName(oldJobName)
+	nextBuildNumber=job.getNextBuildNumber()
+	//Update next build number only for 'master' branch job as BakeIamge runs only for master/develop branch
+	job = Jenkins.instance.getItemByFullName(newJobName+"/master")
+	job.nextBuildNumber = nextBuildNumber
+	job.saveNextBuildNumber()
+
 
